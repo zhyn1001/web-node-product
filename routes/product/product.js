e4d25dc4cce0801840e64094a7c35b9c303be03d
zhangyong <feng201340405120@126.com>
2019-04-28 17:35:33 +0800

    修改商品列表

--- routes/product/product.js
+++ routes/product/product.js
@@ -19,16 +19,23 @@ router.get('/', function (req, res, next) {
 
 router.get('/modify/:productID', (req, res, next) => {
 	Product.find({productID:req.params.productID}, modelback, (err, docs) => {
-		if(docs){
+		if(docs && docs.length === 1){
 			res.render('modify', {
 				title: '修改商品',
-				producrList:docs
+				producrList:docs[0]
 			});
 		}
 	})
 })
 router.post('/save', (req, res, next) => {
-
+	Product.updateOne({productID:req.body.productID}, req.body, function(err, result){
+		if(err) {
+			console.log(err)
+		} else {
+			res.set('Content-Type','text/html');
+			res.send(`<script>alert("修改成功");history.back();</script>`)
+		}
+	})
 })
 
 module.exports = router;
