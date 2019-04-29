2cb7d6ee32f28ae5ea6001b3366b527f71702054
zhangyong <feng201340405120@126.com>
2019-04-29 17:45:34 +0800

    上传进度插件

--- routes/import.js
+++ routes/import.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var Product = require('../models/product')
+var progressStream = require('progress-stream');
 
 
 /* 上传*/
