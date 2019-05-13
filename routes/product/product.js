0d2c70251b93fd55ddf3193a2659a70ad2f41bed
zhangyong <zhangyong13@xdf.cn>
2019-05-13 15:21:26 +0800

    新增产品

--- routes/product/product.js
+++ routes/product/product.js
@@ -21,7 +21,11 @@ router.get('/', function (req, res, next) {
 		}
 	})
 });
-
+router.get('/addpage', function(req, res, next){
+	res.render('addpage',{
+		title: '增加商品'
+	})
+})
 router.get('/modify/:productID', (req, res, next) => {
 	Product.find({productID:req.params.productID}, modelback, (err, docs) => {
 		if(docs && docs.length === 1){
@@ -32,35 +36,79 @@ router.get('/modify/:productID', (req, res, next) => {
 		}
 	})
 })
+//新增/编辑
 router.post('/save', upload.single('file'), (req, res, next) => {
-	var files = req.file //文件对象
-	var uploadedPath = files.path;//上传文件路径
-	var dstPath = './public/upload/' + files.originalname;
-	//重命名为真实文件名
-	fs.rename(uploadedPath, dstPath, function(err) {
-		if (err) {
-			console.log('rename error: ' + err);
-		} else {
-			console.log('rename ok');
-		}
-	});
-	
-	let resultList = {
-		productID:req.body.productID,
-		productName:req.body.productName,
-		productPic:files.originalname,
-		price:req.body.price,
-		counts:req.body.counts
+	let productType = req.body.pType;
+	let picUrl = false;
+	if(req.file){
+		var files = req.file //文件对象
+		var uploadedPath = files.path;//上传文件路径
+		var dstPath = './public/upload/' + files.originalname;
+		picUrl = files.originalname
+		//重命名为真实文件名
+		fs.rename(uploadedPath, dstPath, function(err) {
+			if (err) {
+				console.log('rename error: ' + err);
+			} else {
+				console.log('rename ok');
+			}
+		});
 	}
-	console.log(resultList)
-	Product.updateOne({productID:req.body.productID}, resultList, function(err, result){
-		if(err) {
-			console.log(err)
-		} else {
-			res.set('Content-Type','text/html');
-			res.send(`<script>alert("修改成功");</script>`)
+	let resultList = null;
+	let paramArr = ['productID','productName','productPic','price']
+	if(picUrl){
+		resultList = {
+			productID:req.body.productID ? req.body.productID : randomCode(),
+			productName:req.body.productName,
+			productPic:picUrl,
+			price:req.body.price,
+			counts:req.body.counts
 		}
-	})
+	} else {
+		resultList = {
+			productID:req.body.productID ? req.body.productID : randomCode(),
+			productName:req.body.productName,
+			price:req.body.price,
+			counts:req.body.counts
+		}
+	}
+	if(productType === 'modify') {
+		Product.updateOne({productID:req.body.productID}, resultList, function(err, result){
+			if(err) {
+				console.log(err)
+			} else {
+				res.set('Content-Type','text/html');
+				res.send(`<script>alert("修改成功");</script>`)
+			}
+		})
+	} else if(productType === 'add'){
+		var pdt = new Product({
+			productID:randomCode(),
+			productName:req.body.productName,
+			productPic:picUrl,
+			price:req.body.price,
+			counts:req.body.counts
+		})
+		pdt.save(function(err, result){
+			if(err) {
+				console.log(err)
+			} else {
+				res.set('Content-Type','text/html');
+				res.send(`<script>alert("新增成功");</script>`)
+			}
+		})
+	}
 })
 
+function randomCode(){
+	let reg = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+	let str = '';
+	let len = 8;
+	for(let i=0; i<len; i++){
+		let pos = Math.round(Math.random() * (reg.length-1))
+		str += reg[pos]
+	}
+	return str;
+}
 module.exports = router;
